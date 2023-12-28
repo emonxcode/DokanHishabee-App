e106894b548c3d87c1722bc34782343b7803a75e
emonxcode <shahriar3215emon@gmail.com>
2023-12-28 22:27:14 +0600

    database table update

--- lib/src/constants/api_endpoints.dart
+++ lib/src/constants/api_endpoints.dart
@@ -1,18 +1,6 @@
 class ApiUrl {
-  static const String baseUrl = 'http://10.0.2.2:2023/api/';
+  static const String baseUrl = 'http://10.0.2.2:2024/api/';
   static String loginUrl = '${baseUrl}user/login';
-  static String registerUrl = '${baseUrl}user/register';
-  static String getProjectsByUserId = '${baseUrl}project/getProjectsByUserId/';
-  static String getContributions =
-      '${baseUrl}collaboration/getCollaborationProjects';
-  static String getContributors =
-      '${baseUrl}collaboration/getCollaboratorsByProjectId';
-  static String getTasksUrl = '${baseUrl}task/getTasks';
-  static String getUserByEmail = '${baseUrl}user/getUserByEmail';
-  static String createCollaborationUrl =
-      '${baseUrl}collaboration/createCollaboration';
-  static String updateTaskStatusUrl = '${baseUrl}task/updateTaskStatus/';
-  static String updateTaskCollaborationUrl =
-      '${baseUrl}task/updateTaskCollatoraion/';
-  static String createTaskUrl = "${baseUrl}task/createTask";
+  static String categoryUrl = '${baseUrl}getAllCategory';
+  static String createCategoryUrl = '${baseUrl}createCategory';
 }
